d02f04ca028b580a78ff04a6f940d3320ad088df
Mauro Fontana <mfontana@df.uba.ar>
2022-05-09 14:09:31 -0300

    Fixed typo in python binary plotting sample script.

--- contrib/python/plot_binary.py
+++ contrib/python/plot_binary.py
@@ -15,13 +15,13 @@ nx, ny, nz = x.size, y.size, z.size
 shape = (nx, ny, nz)
 
 # Reads binary files
-# For simulations in single precision change np.float32
+# For simulations in double precision change np.float32
 # to np.float64
 vx = np.fromfile(path+'out/vx.0001.out',dtype=np.float32).reshape(shape,order='F')
 
 # Show a vertical cut of the field in the middle of the box
 plt.figure()
-plt.imshow(vx[:,ny//2,:].T, origin='lower', extents=[ x[0],x[-1],z[0],z[-1] ],
+plt.imshow(vx[:,ny//2,:].T, origin='lower', extent=[ x[0],x[-1],z[0],z[-1] ],
            interpolation='gaussian')
 plt.xlabel('x')
 plt.ylabel('z')
